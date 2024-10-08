9e69d382757e7a02598f1a847cf6357ccfa2ef19
Fabian Döz <fabian@doez.info>
2024-10-08 20:34:34 +0200

    add some style and set min value for hydration and quantity

--- App.js
+++ App.js
@@ -14,32 +14,47 @@ export default function App() {
 		setWater(1.66667 * quantity * hydration);
 		setSalt(3.33333 * quantity);
 		setYeast(0.33333 * quantity);
+		if (quantity < 0) {
+			setQuantity(0);
+		}
+		if (hydration < 60) {
+			setHydration(60);
+		}
 	}
 	useEffect(() => {
 		calculate();
 	}, [quantity, hydration]);
 	return (
 		<View style={styles.container}>
-			<Text style={styles.headline}>Pizza Calculator</Text>
+			<Text style={styles.headline}>Pizza Calculator 🍕</Text>
 			<View>
 				<View style={styles.option}>
-					<Text>Menge (Quantity)</Text>
-					<Text>{quantity}</Text>
-					<Button title="+" onPress={() => setQuantity(quantity + 1)}/>
-					<Button title="-" onPress={() => setQuantity(quantity - 1)}/>
+					<Text>Menge</Text>
+					<Text>{quantity >= 0 ? quantity : 0}</Text>
+					<View style={styles.button}>
+						<Button style={styles.button} title="-" onPress={() => setQuantity(quantity - 1)}/>
+					</View>
+					<View style={styles.button}>
+						<Button style={styles.button} title="+" onPress={() => setQuantity(quantity + 1)}/>
+					</View>
+
 				</View>
 				<View style={styles.option}>
 					<Text>Hydration (%)</Text>
-					<Text>{hydration}</Text>
-					<Button title="+" onPress={() => setHydration(hydration + 1)}/>
-					<Button title="-" onPress={() => setHydration(hydration - 1)}/>
+					<Text>{hydration >= 60 ? hydration : 60}</Text>
+					<View style={styles.button}>
+						<Button title="-" onPress={() => setHydration(hydration - 1)}/>
+					</View>
+					<View style={styles.button}>
+						<Button style={styles.button} title="+" onPress={() => setHydration(hydration + 1)}/>
+					</View>
 				</View>
 			</View>
 			<View>
-				<Text>Flour: {flour.toFixed(2)}g</Text>
-				<Text>Water: {water.toFixed(2)}g</Text>
-				<Text>Salt: {salt.toFixed(2)}g</Text>
-				<Text>Yeast: {yeast.toFixed(2)}g</Text>
+				<Text>Mehl: {flour.toFixed(2)}g</Text>
+				<Text>Wasser: {water.toFixed(2)}g</Text>
+				<Text>Salz: {salt.toFixed(2)}g</Text>
+				<Text>Hefe: {yeast.toFixed(2)}g</Text>
 				<StatusBar style="auto"/>
 			</View>
 		</View>
@@ -47,30 +62,30 @@ export default function App() {
 }
 const styles = StyleSheet.create({
 	container: {
+		paddingTop: 70,
 		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
 		display: 'flex',
 		gap: 20,
 	},
 	headline: {
-		fontSize: 20,
+		fontSize: 25,
 		fontWeight: 'bold',
-	},
-	input: {
-		height: 40,
-		borderColor: 'gray',
-		borderWidth: 1,
-		marginBottom: 12,
-		paddingHorizontal: 8,
-		width: '80%',
-		textAlign: 'center',
+		color: 'red',
 	},
 	option: {
 		display: 'flex',
 		flexDirection: 'row',
 		gap: 12,
 		alignItems: 'center',
+		marginBottom: 12,
+	},
+	button: {
+		borderRadius: 4,
+		backgroundColor: 'lightgray',
+		borderColor: 'red',
+		borderWidth: 1,
 	},
 });
